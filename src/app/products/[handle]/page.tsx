092c05f3c8e68f2ca34c64f207f0eb4c7a848f90
Alex <alexgmark@gmail.com>
2025-11-25 21:52:05 +0000

    Ugly fix for PageProps needing promise, must be missing something elsewhere

--- src/app/products/[handle]/page.tsx
+++ src/app/products/[handle]/page.tsx
@@ -1,14 +1,16 @@
+// src/app/products/[handle]/page.tsx
+
 import { getShopifyProduct } from '@/lib/shopify/queries/getProductByHandle';
 import ProductDetails from '@/components/sections/ProductDetails';
 
-type Props = {
-  params: {
-    handle: string;
-  };
+type Params = Promise<{ handle: string }>;
+
+type ProductPageProps = {
+  params: Params;
 };
 
-export default async function ProductPage(props: Props) {
-  const { handle } = await props.params;
+export default async function ProductPage({ params }: ProductPageProps) {
+  const { handle } = await params;
 
   const productData = await getShopifyProduct(handle);
   const product = productData?.productByHandle;
